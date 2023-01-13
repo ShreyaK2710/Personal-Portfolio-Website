045c2ace58a3843c331f5c9d3b92c4d98f7fa92d
Shreya K <95535050+ShreyaK2710@users.noreply.github.com>
2023-01-13 10:46:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+(function ($) {
+    "use strict";
+
+    // Typed Initiate
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+
+})(jQuery);
+
